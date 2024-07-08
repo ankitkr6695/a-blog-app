85406b89a1a74c684c12cb1119ca07d0c8c540d3
Ankit Kumar <krankit6695@gmail.com>
2024-07-08 17:18:37 +0530

    adding project files

---
+++ src/component/index.js
@@ -0,0 +1,32 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "./Signup";
+import Login from "./Login"
+import AddPost from "../pages/AddPost";
+import PostForm from './post_form/PostForm'
+import PostCard from './PostCard'
+import Button from './Button'
+import AuthLayout from './AuthLayout'
+import Input from './Input'
+import Select from "./Select";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    SignUp,
+    Login,
+    AddPost,PostForm,
+    PostCard,
+    Button,
+    AuthLayout,
+    Input,
+    Select
+}
